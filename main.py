d666c1795b5596ef1e2c911060e28bbb61b83f09
ABojo <Alex.bojorquez2598@gmail.com>
2025-03-25 04:00:30 -0400

    Setup basic CLI argument parsing

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="A script for downloading the videos on a BlueSky profile.")
+
+    parser.add_argument('username', type=str, help='The username of the profile you want to download videos from.')
+    parser.add_argument('-f', '--folder', type=str, help='The folder you wish to store the videos in.', required=False)
+
+    args = parser.parse_args()
+    
 if __name__ == "__main__":
-    pass
+    main()
